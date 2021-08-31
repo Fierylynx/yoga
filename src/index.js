9df7d946019c5c8d0e2f4f0e60b6fce36a7c240b
Oleksandr Ruban <fierylynx2016@gmail.com>
2021-08-31 17:48:18 +0200

    адаптив desktop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 window.$ = window.jQuery = require("jquery");
+
+
+
+$(window).on("scroll", function() {
+  var scrollPos = $(window).scrollTop();
+  if (scrollPos <= 0) {
+      $('.header').removeClass('header--scroll');
+  } else {
+      $('.header').addClass('header--scroll');
+  }
+});
