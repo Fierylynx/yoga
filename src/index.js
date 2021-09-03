6ed1c2e8901ed31da553b0322b2bb3c72f48054e
Oleksandr Ruban <fierylynx2016@gmail.com>
2021-09-03 19:42:57 +0200

    added swiper

--- src/index.js
+++ src/index.js
@@ -10,3 +10,41 @@ $(window).on("scroll", function() {
       $('.header').addClass('header--scroll');
   }
 });
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.team__pagination',
+    type: 'fraction',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.team__button-next',
+    prevEl: '.team__button-prev',
+  },
+});
+
+const swiperReviews = new Swiper('.reviews__slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+
+  // If we need pagination
+  pagination: {
+    el: '.reviews__pagination',
+    type: 'fraction',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews__button-next',
+    prevEl: '.reviews__button-prev',
+  },
+});
